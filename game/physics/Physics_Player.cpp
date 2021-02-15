ffa6aed04315fa375277516977a1e7823621c4fc
Daniel Gibson <metalcaedes@gmail.com>
2021-02-15 05:27:37 +0100

    Fix player's clipModel->axis when loading savegame, fixes #328
    idClipModel::axis is an idMat3 rotation matrix.
    Usually it's an identity matrix, but if the player is pushed around by
    an idPush entity it's modified and apparently can (wrongly) remain
    modified, possibly when saving while idPush is active.
    This seems to happen sometimes on the crashing elevator in game/delta1.
    The fix/workaround is to reset it to mat3_identity when loading a
    savegame.

--- game/physics/Physics_Player.cpp
+++ game/physics/Physics_Player.cpp
@@ -1460,7 +1460,7 @@ void idPhysics_Player::MovePlayer( int msec ) {
 		}
 		//gameLocal.Printf("upspeed %f\n", upspeed);
 
-		//se non siamo a terra o la nostra velocità ci spara su -> casca secondo gravità
+		//se non siamo a terra o la nostra velocitï¿½ ci spara su -> casca secondo gravitï¿½
 		if ( (animMoveGravityMultiplier != 0) && ( !groundPlane || upspeed > 1.0f ) ) { //to do: use HasGroundContacts() instead of groundPlane?
 			//delta = current.velocity * frametime; //ignora il delta richiesto
 			//gameLocal.Printf("delta.z %f\n", delta.z);
@@ -1476,7 +1476,7 @@ void idPhysics_Player::MovePlayer( int msec ) {
 				delta.Zero(); //job done, reset it
 			}
 			current.velocity += gravityVector * frametime * animMoveGravityMultiplier;
-		} else { //siamo a terra or gravità disabilitata -> fai l'animazione e basta
+		} else { //siamo a terra or gravitï¿½ disabilitata -> fai l'animazione e basta
 			current.velocity = delta / frametime;
 			current.velocity -= ( current.velocity * gravityNormal ) * gravityNormal;
 
@@ -1734,6 +1734,20 @@ void idPhysics_Player::Restore( idRestoreGame *savefile ) {
 	savefile->ReadObject( reinterpret_cast<idClass *&>( blockingEntity ) ); //ivan
 	//savefile->ReadFloat( animMoveGravityMultiplier ); //ivan
 	//savefile->ReadBool( animMoveUseGravity ); //ivan
+
+	/* DG: It can apparently happen that the player saves while the clipModel's axis are
+	 *     modified by idPush::TryRotatePushEntity() -> idPhysics_Player::Rotate() -> idClipModel::Link()
+	 *     Normally idPush seems to reset them to the identity matrix in the next frame,
+	 *     but apparently not when coming from a savegame.
+	 *     Usually clipModel->axis is the identity matrix, and if it isn't there's clipping bugs
+	 *     like CheckGround() reporting that it's steep even though the player is only trying to
+	 *     walk up normal stairs.
+	 *     Resetting the axis to mat3_identity when restoring a savegame works around that issue
+	 *     and makes sure players can go on playing if their savegame was "corrupted" by saving
+	 *     while idPush was active. See https://github.com/dhewm/dhewm3/issues/328 for more details */
+	if ( clipModel != nullptr ) {
+		clipModel->SetPosition( clipModel->GetOrigin(), mat3_identity );
+	}
 }
 
 /*

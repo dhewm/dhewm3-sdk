e18f02d6a6f2007cf2096292a810031bfea4fef2
Daniel Gibson <metalcaedes@gmail.com>
2026-01-06 23:39:23 +0100

    Clip.cpp TestHugeTranslation(): Disable annoying assertion
    and turn the prints into warnings
    
    moved the commented out assertions to where they were before
    commit c22965bf580f0a718b7117a92da83205fccc32ec
    so the code is a bit closer to the original (and the SDK)

--- game/physics/Clip.cpp
+++ game/physics/Clip.cpp
@@ -967,6 +967,9 @@ idClip::TestHugeTranslation
 */
 ID_INLINE bool TestHugeTranslation( trace_t &results, const idClipModel *mdl, const idVec3 &start, const idVec3 &end, const idMat3 &trmAxis ) {
 	if ( mdl != NULL && ( end - start ).LengthSqr() > Square( CM_MAX_TRACE_DIST ) ) {
+
+		// assert( 0 ); DG: this was annoying and not really necessary, a Warning should suffice.
+
 		results.fraction = 0.0f;
 		results.endpos = start;
 		results.endAxis = trmAxis;
@@ -976,16 +979,13 @@ ID_INLINE bool TestHugeTranslation( trace_t &results, const idClipModel *mdl, co
 
 		if ( mdl->GetEntity() ) {
 			//SPAM in Delta labs sector 1 (client-side) by some stupid carton box, FIXME
-			gameLocal.Printf( "huge translation for clip model %d on entity %d '%s'\n", mdl->GetId(), mdl->GetEntity()->entityNumber, mdl->GetEntity()->GetName() );
+			gameLocal.Warning( "huge translation for clip model %d on entity %d '%s'\n", mdl->GetId(), mdl->GetEntity()->entityNumber, mdl->GetEntity()->GetName() );
 		} else {
-			gameLocal.Printf( "huge translation for clip model %d\n", mdl->GetId() );
+			gameLocal.Warning( "huge translation for clip model %d\n", mdl->GetId() );
 		}
 
-		gameLocal.Printf( "  from (%.2f %.2f %.2f) to (%.2f %.2f %.2f)\n", start.x, start.y, start.z, end.x, end.y, end.z);
+		gameLocal.Warning( "  from (%.2f %.2f %.2f) to (%.2f %.2f %.2f)\n", start.x, start.y, start.z, end.x, end.y, end.z);
 
-		if (!gameLocal.mpGame.IsGametypeCoopBased()) { //Avoid crash for clients in coop (and in rare cases for server), ROE did the same in CTF so...
-			assert( 0 );
-		}
 		return true;
 	}
 	return false;

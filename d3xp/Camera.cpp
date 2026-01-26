c27ef6daa0166058b919dc96c2297ce727f0a5da
Daniel Gibson <metalcaedes@gmail.com>
2026-01-26 04:51:33 +0100

    Introduce gameLocal.msecPrecise to d3xp/
    basically the same as in the previous commit, but more complicated
    due to d3xp's slowmo feature.
    slowmoMsec was always a float, now its value is based on the precise
    frametime (16.666ms, added USERCMD_MSEC_PRECISE for that) instead of
    USERCMD_MSEC = 16

--- d3xp/Camera.cpp
+++ d3xp/Camera.cpp
@@ -630,7 +630,7 @@ void idCameraAnim::Think( void ) {
 
 		if ( frameRate == USERCMD_HZ ) {
 			frameTime	= realUserTime - starttime;
-			frame		= frameTime / gameLocal.msec;
+			frame		= frameTime / gameLocal.msecPrecise;
 		} else {
 			frameTime	= ( realUserTime - starttime ) * frameRate;
 			frame		= frameTime / 1000;
@@ -686,7 +686,7 @@ void idCameraAnim::GetViewParms( renderView_t *view ) {
 
 	if ( frameRate == USERCMD_HZ ) {
 		frameTime	= realUserTime - starttime;
-		frame		= frameTime / gameLocal.msec;
+		frame		= frameTime / gameLocal.msecPrecise;
 		lerp		= 0.0f;
 	} else {
 		frameTime	= ( realUserTime - starttime ) * frameRate;

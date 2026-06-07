a4dfc7ca5f338df50eac83806e123f88121d58a5
Daniel Gibson <metalcaedes@gmail.com>
2026-06-08 00:43:44 +0200

    Fix build with compilers that default to C++20
    by telling them to use C++98, which should suffice for game DLLs.
    Consequently, disabled warnings about C++20 conflicts (mostly about
    variables called "requires" whichc is a keyword in C++20).
    
    Still removed superfluous template arguments in idList and idStaticList
    destructors (illegal in C++20, pointless in other versions)

--- idlib/containers/StaticList.h
+++ idlib/containers/StaticList.h
@@ -44,7 +44,7 @@ public:
 
 						idStaticList();
 						idStaticList( const idStaticList<type,size> &other );
-						~idStaticList<type,size>( void );
+						~idStaticList( void );
 
 	void				Clear( void );										// marks the list as empty.  does not deallocate or intialize data.
 	int					Num( void ) const;									// returns number of elements in list
